78ba621a4afed2980cce0a7f3d643e018354d117
thotachenchubalaji <153853940+thotachenchubalaji@users.noreply.github.com>
2023-12-14 23:44:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+function changeImage(clickedImage){
+var newSrc = clickedImage.src;
+      var oldWidth = targetImage.width;
+      var oldHeight = targetImage.height;
+      targetImage.src = newSrc;
+      targetImage.width = oldWidth;
+      targetImage.height = oldHeight;
+}
+
+function drake(clickedVid){
+  var newVid='video/drake.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+function Ye(clickedVid){
+  var newVid='video/ye.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+
+function travis(clickedVid){
+  var newVid='video/travis.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+
+function weeknd(clickedVid){
+  var newVid='video/weeknd.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+
+function future(clickedVid){
+  var newVid='video/future.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+
+function metro(clickedVid){
+  var newVid='video/metro.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+function don(clickedVid){
+  var newVid='video/don.mp4';
+  var oldWidthvid = targetVid.width;
+  var oldHeighvid = targetVid.height;
+  targetVid.src=newVid;
+  targetVid.width = oldWidthvid;
+  targetVid.height = oldHeightvid;
+}
+var backgroundMusic = document.getElementById("backgroundMusic");
+
+function playBackgroundMusic() {
+  backgroundMusic.play();
+}
+
+function pauseBackgroundMusic() {
+  backgroundMusic.pause();
+}
